6dbb67c73a4b93379a026f31ee94a94b2bba94fb
Jimmy Fung <jimmythefung@yahoo.com>
2024-07-13 09:25:22 -0400

    expanded seeding

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # course-reg
+
 Rutgers full stack demo
 
 # Helpful Commands
+
 ## Prisma
-  * `npx prisma db push`  
-  * `npx prisma studio`
-  * `npx prisma db seed`
+
+-   `npx prisma db push`
+-   `npx prisma studio`
+-   `npx prisma db seed`
+
+### Re-seed DB
+
+```bash
+rm prisma/dev.db; npx prisma db push; npx prisma db seed
+```
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,22 +1,100 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+const list_of_classes = [
+    {
+        code: "01:198:105",
+        name: "Great Insights in Computer Science",
+        subject: "Computer Science",
+        term: "Fall 2024",
+    },
+    {
+        code: "01:198:107",
+        name: "Computing for Math and the Sciences",
+        subject: "Computer Science",
+        term: "Fall 2024",
+    },
+    {
+        code: "01:198:110",
+        name: "Principles of Computer Science",
+        subject: "Computer Science",
+        term: "Fall 2024",
+    },
+    {
+        code: "01:198:111",
+        name: "Intro Computer Sci",
+        subject: "Computer Science",
+        term: "Fall 2024",
+    },
+    {
+        code: "01:198:112",
+        name: "Data Structures",
+        subject: "Computer Science",
+        term: "Fall 2024",
+    },
+    {
+        code: "01:640:491",
+        name: "Mathematics Problem Solving Seminar",
+        subject: "Math",
+        term: "Fall 2024",
+    },
+];
+
+const list_of_students = [
+    {
+        studentid: "aa2024",
+        name: "Alice",
+        major: "Accounting",
+        graduation: "2024-05-20T00:00:00.000Z",
+    },
+    {
+        studentid: "ba2025",
+        name: "Bob",
+        major: "Biology",
+        graduation: "2024-05-20T00:00:00.000Z",
+    },
+    {
+        studentid: "jd2026",
+        name: "Jane",
+        major: "Journalism",
+        graduation: "2024-05-20T00:00:00.000Z",
+    },
+];
+
 async function main() {
     // Class
-    const c1 = await prisma.class.upsert({
-        where: {
-            code: "01:640:491",
-        },
-        update: {},
-        create: {
-            code: "01:640:491",
-            name: "Mathematics Problem Solving Seminar",
-            subject: "Math",
-            term: "Fall 2024",
-        },
-    });
+    const collection_classes = await prisma.$transaction(
+        list_of_classes.map((c) =>
+            prisma.class.upsert({
+                where: { code: c.code },
+                update: {},
+                create: {
+                    code: c.code,
+                    name: c.name,
+                    subject: c.subject,
+                    term: c.term,
+                },
+            })
+        )
+    );
 
-    // Student
+    // Students
+    const collection_student = await prisma.$transaction(
+        list_of_students.map((s) =>
+            prisma.student.upsert({
+                where: { studentid: s.studentid },
+                update: {},
+                create: {
+                    studentid: s.studentid,
+                    name: s.name,
+                    major: s.major,
+                    graduation: s.graduation,
+                },
+            })
+        )
+    );
+
+    // A student with Relation to enrollment
     const s1 = await prisma.student.upsert({
         where: {
             studentid: "jf1128",
@@ -41,7 +119,7 @@ async function main() {
         },
     });
 
-    console.log({ s1, c1 });
+    console.log({ s1 });
 }
 
 // execute the main function
